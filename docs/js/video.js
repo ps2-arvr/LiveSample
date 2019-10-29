7cf7d3998c6023ccf3cdb065278afa43e6155e8b
ps2-arvr <progressive.system2@gmail.com>
2019-10-29 14:05:59 +0900

    add to index.html

---
+++ docs/js/video.js
@@ -0,0 +1,26 @@
+class Video {
+
+	init() {
+	
+		// フロント（イン）カメラの場合	
+		const medias = {audio : false, video : { facingMode: "user" }},
+ 		video  = document.getElementById("video");
+
+		// リア（バック/アウト）カメラの場合
+		/* const medias = {audio : false, video : { facingMode: { exact: "environment" } }},
+ 		     video  = document.getElementById("video"); */
+		
+		navigator.getUserMedia(medias, successCallback, errorCallback);	
+
+	
+function successCallback(stream) {
+		  //video.srcObject = stream;
+		video.src = window.URL.createObjectURL( stream );
+		// 動画の再生
+		peer.addStream(stream)
+		video.play();
+		}
+
+function errorCallback(error) {
+		  alert(error);
+		}
